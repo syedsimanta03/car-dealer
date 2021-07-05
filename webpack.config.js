305dba6396c196dfc9b2606e9cd904aa3855637c
sam <syed.simanta06@gmail.com>
2021-07-05 21:31:51 +0600

    process

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    })
-],
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
 }
