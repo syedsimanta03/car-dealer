4dc26b4aba9738f1b76c4bae096de095aaa415c2
sam <syed.simanta06@gmail.com>
2021-07-05 20:54:55 +0600

    fixedall

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:😢'));
 
 // GraphQL use Graphical UI
 //app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
 
 // Connect schema to GraphQL
 app.use(
