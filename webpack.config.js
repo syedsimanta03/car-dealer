7a8dede85c70f7bdb494389e85d65215d5c0351c
sam <syed.simanta06@gmail.com>
2021-07-17 22:30:28 +0600

    cg

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 // webpack.config.js
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
-  plugins: [new Dotenv({ systemvars: true })],
+  plugins: [
+    new Dotenv({ systemvars: true }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
 }
