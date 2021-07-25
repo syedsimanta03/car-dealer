31f14e238d10c7307a5a25fa20782f696aca711b
sam <syed.simanta06@gmail.com>
2021-07-25 23:31:51 +0600

    path

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
   })
 
 }
