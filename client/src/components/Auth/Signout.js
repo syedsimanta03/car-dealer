e77a01511f0553d109feb0d52b3af09ca10542f9
syedsimanta03 <syed.simanta06@gmail.com>
2022-04-30 23:58:20 +0600

    'added'

--- client/src/components/Auth/Signout.js
+++ client/src/components/Auth/Signout.js
@@ -1,31 +1,37 @@
-import React from 'react';
-import { ApolloConsumer } from 'react-apollo';
-import { withRouter } from 'react-router-dom';
-import Notiflix from 'notiflix-react';
-
-// Init the module you want to use. e.g. Notify Module
-Notiflix.Notify.Init({});
+import React from 'react'
+import { ApolloConsumer } from 'react-apollo'
+import { withRouter } from 'react-router-dom'
+import { notification, Icon } from 'antd'
 
 const handleSignout = (client, history) => {
   // clear local storage token
-  localStorage.setItem('token', '');
+  localStorage.setItem('token', '')
   // Apollo client store reset->signout successfully
-  client.resetStore();
+  client.resetStore()
   // Redirect to Home after Signout
-  history.push('/');
+  history.push('/')
   // Show notification
-  Notiflix.Notify.Success('Logged Out');
-};
+  notification.open({
+    message: 'Logged Out From The App',
+    description: 'See you soon!',
+    icon: <Icon type='check-circle' theme='twoTone' twoToneColor='#52c41a' />,
+  })
+}
 
 const Signout = ({ history }) => (
   <ApolloConsumer>
-    {client => {
+    {(client) => {
       // console.log(client);
       return (
-        <button className="signout" onClick={() => handleSignout(client, history)}>Signout</button>
-      );
+        <button
+          className='signout'
+          onClick={() => handleSignout(client, history)}
+        >
+          Signout
+        </button>
+      )
     }}
   </ApolloConsumer>
-);
+)
 
-export default withRouter(Signout);
+export default withRouter(Signout)
